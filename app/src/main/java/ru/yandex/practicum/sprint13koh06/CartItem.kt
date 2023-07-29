ba143e9e319c47427e1301881d1b3d0c8faca890
Mark Quarasique <julipaha@gmail.com>
2023-07-29 11:02:11 +0300

    Revert "10. Добавлено отображение суммы элемента корзины"
    This reverts commit 478085ed9b71f45580dabdb2b5974efbad491291.

--- app/src/main/java/ru/yandex/practicum/sprint13koh06/CartItem.kt
+++ app/src/main/java/ru/yandex/practicum/sprint13koh06/CartItem.kt
@@ -1,13 +1,10 @@
 package ru.yandex.practicum.sprint13koh06
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import ru.yandex.practicum.sprint13koh06.databinding.VCartItemBinding
-import java.text.NumberFormat
-import java.util.Locale
 
 data class CartItem(
     val id: String,
@@ -31,6 +28,7 @@ class CartItemViewHolder(
 ) {
 
     fun bind(item: CartItem) {
+        binding.root
 
         Glide
             .with(binding.root.context)
@@ -39,11 +37,6 @@ class CartItemViewHolder(
         binding.title.text = item.catalogItem.name
 
         binding.count.text = item.count.toString()
-        val format = NumberFormat.getCurrencyInstance(Locale.forLanguageTag("RU"))
-        val priceF = format.format((item.catalogItem.price.toDouble() * item.count)/100)
-        val priceS = priceF.substring(0, priceF.length-2)
-        val currencyChar = binding.root.context.getString(R.string.currency)
-        binding.totalCost.text = "$priceS $currencyChar"
     }
 
 }
